a3c5c10960de6284d70ffcf35cf3be45251d3562
JonssonAlexander <112644594+JonssonAlexander@users.noreply.github.com>
2022-12-30 12:37:29 +0100

    Update microblog.py

--- Flask Website/microblog.py
+++ Flask Website/microblog.py
@@ -12,8 +12,6 @@ def make_shell_context():
 #export MAIL_SERVER=smtp.googlemail.com export MAIL_PORT=587 export MAIL_USE_TLS=1 export MAIL_USERNAME=<your-gmail-username> export MAIL_PASSWORD=<your-gmail-password>
 # export MAIL_SERVER=smtp.googlemail.com
 # export MAIL_PORT=587
-# export MAIL_USERNAME=kungfjjfjfjfj@gmail.com   
-# export MAIL_PASSWORD=hejsan123 
 # export MAIL_USE_TLS=1
 
 #OM LOCALHOST: python -m smtpd -n -c DebuggingServer localhost:8025 
